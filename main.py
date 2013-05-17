6cb4da86f48ba8af44b7b3cc86d7af2fc3d77876
bob_boulot <bob_boulot@daribouca.net>
2013-05-17 11:10:31 +0100

    rewrote preliminary test routines to check for dummy *.miz file in a relative "missions_in" directory instead of hard-coded paths

--- main.py
+++ main.py
@@ -11,55 +11,49 @@
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 # coding=utf-8
-import mizfile, logging, slpp, _slpp, filecmp, difflib, mission
-from os import listdir
-from os.path import join
+import mizfile, logging, _slpp, filecmp, difflib, mission, os
 from _logging import mkLogger, logged
 logger = mkLogger(__name__, logging.INFO )
 
-mizPath1 = r"C:\Documents and Settings\owner\My Documents\BORIS\TDCMEMEv2.git\missions\1.2.4\BenJee"
-mizFiles = (join(mizPath1, file) for file in listdir(mizPath1) if file[-4:] == ".miz")
-
-testMizFile1 = r"C:\Documents and Settings\owner\My Documents\BORIS\TDCMEMEv2.git\missions\1.2.4\BenJee\F-15C - Justice.miz"
-testMissionFile1 = r"C:\Documents and Settings\owner\My Documents\BORIS\TDCMEMEv2.git\missions\1.2.4\BenJee\mission1"
-testMissionFile2 = r"C:\Documents and Settings\owner\My Documents\BORIS\TDCMEMEv2.git\missions\1.2.4\BenJee\mission2"
-testMissionFile3 = r"C:\Documents and Settings\owner\My Documents\BORIS\TDCMEMEv2.git\missions\1.2.4\BenJee\mission3"
-testMissionFileWrite = r"C:\Documents and Settings\owner\My Documents\BORIS\TDCMEMEv2.git\missions\1.2.4\BenJee\mission_out"
-testMissionDiff =r"C:\Documents and Settings\owner\My Documents\BORIS\TDCMEMEv2.git\missions\1.2.4\BenJee\mission_diff.txt"
-
 def main():
-    with mission.Mission(testMizFile1) as mis:
-        pass
-##    MIZ = mizfile.MizFile(testMizFile1)
-##    mis = MIZ.parse_mission()
-##    logger.info(str(mis))
-    return
+    wk_dir = os.getcwd()
+    missions_in = os.path.join(wk_dir,"missions_in")
+    missions_out = os.path.join(wk_dir, "missions_out")
+    miz_files_in = (os.path.join(missions_in, file) for file in os.listdir(missions_in) if file[-4:] == ".miz")
+    # run tests here
 
-    # Build files list
-    logger.info("Building sample mission files list")
-    for file in mizFiles:
-        logger.debug("".join(["Found files: ", file]))
-        instance = mizfile.MizFile(file)
-        instance.check()
-        instance.decompact()
-        instance.recompact()
-        instance.delete_temp_dir()
+    # one test to rule them all:
+##    for file_path in miz_files_in:
+##        file_path = file_path.replace("\\","\\\\")
+##        print(file_path)
+    run_on_all_files(generate_context,miz_files_in)
 
-    return
+def SLPP_test(file_in, file_out):
+    '''
+    Vérifie que le parser SLPP n'altère pas la table LUA qui lui est passée en
+    entrée
+
+    BUGGED: cfr. gh-1
+    '''
     p = _slpp.SLPP()
+    with open(file_out, mode="w", encoding="UTF-8") as _out:
+        with open(file_in, encoding="UTF-8") as _in:
+            dict_in = p.decode(_in.read())
+            _out.write(p.encode(dict_in))
 
-    with open(testMissionFileWrite, mode="w", encoding="UTF-8") as file_out:
-        with open(testMissionFile3, encoding="UTF-8") as file_in:
-            dict_in = p.decode(file_in.read())
-            file_out.write(p.encode(dict_in))
-
-    with open(testMissionFileWrite, mode="r", encoding="UTF-8") as file_out:
-        dict_out = p.decode(file_out.read())
+    with open(file_out, mode="r", encoding="UTF-8") as _out:
+        dict_out = p.decode(_out.read())
 
     print(dict_in == dict_out)
-    return
 
-##    t = mizfile.MizFile(mizFile1, "tmp")
+def generate_context(file):
+    with mission.Mission(file):
+        pass
+
+def run_on_all_files(function, files):
+    for file in files:
+        print("running function {} on file {}".format(function.__name__,file))
+        function(file)
 
 if __name__ == '__main__':
     main()
