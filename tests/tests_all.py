be4eafba70110132e8ac47bd6f6215a381d4c125
bob_boulot <bob_boulot@daribouca.net>
2013-05-21 22:48:41 +0100

    rewrote the tests to be ok with nosetesting

--- tests/tests_all.py
+++ tests/tests_all.py
@@ -10,14 +10,19 @@
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
-import unittest, tests_slpp, test_ground_unit_resolve
+import unittest, test_ground_unit_resolve
+
 from _logging._logging import mkLogger, logged, DEBUG, INFO
 mkLogger(__name__, INFO)
 import glob
 
-test_file_strings = glob.glob('test*.py')
-module_strings = [str[0:len(str)-3] for str in test_file_strings]
-suites = [unittest.defaultTestLoader.loadTestsFromName(str) for str
-          in module_strings]
-testSuite = unittest.TestSuite(suites)
-text_runner = unittest.TextTestRunner().run(testSuite)
+try:
+    import nose
+    nose.run()
+except ImportError:
+    test_file_strings = glob.glob('test*.py')
+    module_strings = [str[0:len(str)-3] for str in test_file_strings]
+    suites = [unittest.defaultTestLoader.loadTestsFromName(str) for str
+              in module_strings]
+    testSuite = unittest.TestSuite(suites)
+    text_runner = unittest.TextTestRunner().run(testSuite)
--- tests/tests_slpp.py
+++ tests/tests_slpp.py
@@ -7,7 +7,7 @@ import mission
 ##from ..slpp import SLPP
 import os
 from _logging._logging import mkLogger, logged, DEBUG, INFO
-mkLogger(__name__, DEBUG)
+mkLogger(__name__, INFO)
 
 class TestGlobalFunctions(unittest.TestCase):
 
@@ -26,16 +26,16 @@ class TestGlobalFunctions(unittest.TestCase):
 
         To run test, drop any number of *.miz files into the slpp tests directory
         '''
-        miz_files = os.listdir("slpp tests")
-        file_out = os.path.join(os.getcwd(),"slpp tests/output")
+        miz_files = os.listdir("tests/slpp_tests")
+        file_out = os.path.join(os.getcwd(),"slpp_tests/output")
         p = _slpp.SLPP()
         for f in miz_files:
             if not f[-4:] == ".miz":
                 continue
-            path_to_file = os.path.normpath(os.path.join(os.getcwd(),"slpp tests",f))
+            path_to_file = os.path.normpath(os.path.join(os.getcwd(),"tests/slpp_tests",f))
             with mission.Mission(os.path.abspath(path_to_file)) as m:
                 path_to_mission_file = os.path.normpath(os.path.join(m.mizfile.temp_dir, "mission" ))
-                file_in = os.path.join(os.getcwd(),"slpp tests/mission")
+                file_in = os.path.join(os.getcwd(),"tests/slpp_tests/mission")
                 with open(file_out, mode="w", encoding="UTF-8") as _out:
                     with open(file_in, encoding="UTF-8") as _in:
                         _out.write(p.encode(p.decode(_in.read())))
