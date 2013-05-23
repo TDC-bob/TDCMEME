88c841c8054c3f05684d0046118511958e1888d8
bob_boulot <bob_boulot@daribouca.net>
2013-05-23 20:50:15 +0100

    won't remove COVER dir anymore

--- nose.bat
+++ nose.bat
@@ -1,4 +1,3 @@
 @echo off
-rmdir /S /Q COVER
 nosetests --no-skip --verbosity=2 --with-coverage --cover-erase --cover-html --cover-html-dir=COVER --debug-log=nose-run.log --logging-level=INFO --detailed-errors
 pause
