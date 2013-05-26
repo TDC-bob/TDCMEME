df4c56867b6a84f5c4a02339717bc65ab2ee7637
bob_boulot <bob_boulot@daribouca.net>
2013-05-26 08:55:13 +0100

    updated path to update directly into the gh-pages repo

--- nose.bat
+++ nose.bat
@@ -1,3 +1,3 @@
 @echo off
-nosetests --no-skip --verbosity=2 --with-coverage --cover-erase --cover-html --cover-html-dir=COVER --debug-log=nose-run.log --logging-level=INFO --detailed-errors
+nosetests --no-skip --verbosity=2 --with-coverage --cover-erase --cover-html --cover-html-dir="C:\Documents and Settings\owner\My Documents\BORIS\TDC\TDCMEME.pages" --debug-log=nose-run.log --logging-level=INFO --detailed-errors
 pause
